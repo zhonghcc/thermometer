7f145b6f5335918c4bd58a26ce7d39517c13117b
Cory Schires <coryschires@gmail.com>
2011-08-07 19:44:06 -0500

    Oops. Pasted wrong plugin code. Fixed.

--- jquery.thermometer.js
+++ jquery.thermometer.js
@@ -1,103 +1,89 @@
 /**
- * Hot Stepper - jQuery plugin to easily create multistep forms
- * and a thermometer with stages for each step.
- * 
- * Source Code: https://github.com/coryschires/hot_stepper
- * 
- * Copyright (c) 2011 Cory Schires (coryschires.com)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- * Version: 0.1.0
- */
+* Thermometer - jQuery plugin which... TODO: add description
+* 
+* Source Code: https://github.com/coryschires/thermometer
+* 
+* Copyright (c) 2011 Cory Schires (coryschires.com)
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+*
+* Version: 0.1.0
+*/
 
 
 (function($) {
 
-  $.hot_stepper = {
+  $.thermometer = {
     defaults: {
-      fields_valid: function() { return true; },
-      after_transition: function() {},
-      next_button_text: 'Next',
-      prev_button_text: 'Prev'
+      title_markup: false,    // if false, then uses the text of the currently active step (i.e. li.active)
+      active_class: 'active'  // in case you're using something different for your active class (e.g. current)
     }
   }
 
-  $.fn.hot_stepper = function(config) {
+  $.fn.thermometer = function(method) {
+    
+    var config = $.extend({}, $.thermometer.defaults, config);
+    
+    var methods = {
 
-    var config = $.extend({}, $.hot_stepper.defaults, config);
+      init : function(config) {
 
-    return this.each(function() {
-      
-      var form = $(this);
-      var steps = form.find("fieldset");
-      var number_of_steps = steps.size();
-      var submit_button = form.find('input[type="submit"]');
-      var prev_button = form.append("<button class='hs_button prev'>"+ config.prev_button_text +"</button>").find('button.prev');
-      var next_button = form.append("<button class='hs_button next'>"+ config.next_button_text +"</button>").find('button.next');
-      var buttons = form.find('.hs_button');
-      var thermometer = form.before('<ul class="hs_thermometer"></ul>').prev();
-
-      var current_step = function() {
-        return steps.filter(':visible');
-      }
-
-      var transition_step = function(direction) {
-        var curr_step = current_step();
-        curr_step.hide();
-        direction === 'next' ? curr_step.next().show() : curr_step.prev().show();
-      };
-
-      var update_button_visibility = function() {
-        var curr_step = current_step()[0],
-            first_step = steps.first()[0],
-            last_step = steps.last()[0];
-
-        if (curr_step === first_step) {
-          prev_button.hide();
-        } else if (curr_step === last_step) {
-          next_button.hide();
-          submit_button.show();
-        } else {
-          buttons.show();
-          submit_button.hide();
-        }
-      };
-
-      var build_thermometer_steps = function() {
-        steps.each(function() {
-          var step_name = $(this).find('legend').html();
-          thermometer.append('<li>'+ step_name +'</li>');
+        return this.each(function() {
+          
+          var thermometer = $(this).addClass('thermometer');
+          
+          // Add stuff to the DOM
+          thermometer.wrap('<div class="thermometer_wrapper"></div>');
+          thermometer.before('<h4 class="thermometer_current_status"></h4>');
+          thermometer.prepend('<div class="progress_bar"></div>');
+          
+          // Create the thermometer
+          helpers.build_thermometer_title(thermometer);
+          helpers.build_progress_bar(thermometer);
         });
-      };
+      },
       
-      var update_active_class_for_thermometer = function() {
-        var thermometer_steps = thermometer.find('li');
-        var current_step_index = steps.index(current_step());
-        thermometer_steps.removeClass('active');
-        $(thermometer_steps.get(current_step_index)).addClass('active');
-      };
-
-      // setup DOM
-      form.find('legend').hide();
-      submit_button.hide();
-      steps.hide().first().show();
-      prev_button.hide();
-      build_thermometer_steps();
-      update_active_class_for_thermometer();
-
-      buttons.click(function() {
-        var direction = $(this).hasClass('prev') ? 'prev' : 'next';
-        
-        if ( direction === 'prev' || config.fields_valid() ) {
-          transition_step(direction);
-          update_button_visibility();
-          update_active_class_for_thermometer();
-          config.after_transition();
+      // public methods
+      update_active_state: function() {
+        helpers.build_thermometer_title(this);
+        helpers.build_progress_bar(this);
+      }
+    }
+    // private_methods
+    var helpers = {
+      active_step: function(thermometer) {
+        return thermometer.find('.' + config.active_class);
+      },
+      build_thermometer_title: function(thermometer) {
+        var title = thermometer.prev();
+        var text = helpers.active_step(thermometer).text();
+        var markup = config.title_markup ? config.title_markup.replace(/{{active}}/, text) : text;
+        title.html(markup);
+      },
+      build_progress_bar: function(thermometer) {
+        var progress_bar = thermometer.find('.progress_bar');
+        var round_to_nearest_five = function(num) {
+          return (num % 5) >= 2.5 ? parseInt(num / 5) * 5 + 5 : parseInt(num / 5) * 5;
         }
-        return false;
-      });
-    });
+        var percentage_completed = function() {
+          var steps = thermometer.find('li');
+          var active_step_number = steps.index(helpers.active_step(thermometer)) + 1;
+          var final_step = active_step_number === steps.size();
+          return final_step ? 100 : round_to_nearest_five( (active_step_number / steps.size()) * 100 );
+        };
+        progress_bar.attr('class', 'progress_bar progress_' + percentage_completed());
+      }
+    }
+
+
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error( 'Method "' +  method + '" does not exist in thermometer plugin!');
+    }
   }
+
 })(jQuery);
