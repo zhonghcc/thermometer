10cfd9023dff4a7d7838b30a1fd0c51fce72efda
Cory Schires <coryschires@gmail.com>
2012-02-01 21:29:22 -0600

    Use CSS3 to render thermometer rather than sprites.

--- jquery.thermometer.js
+++ jquery.thermometer.js
@@ -1,9 +1,9 @@
 /**
-* Thermometer - jQuery plugin which helps turns any list into 
+* Thermometer - jQuery plugin which helps turns any list into
 * a nice looking progress thermometer
-* 
+*
 * Source Code: https://github.com/coryschires/thermometer
-* 
+*
 * Copyright (c) 2011 Cory Schires (coryschires.com)
 * Dual licensed under the MIT and GPL licenses:
 * http://www.opensource.org/licenses/mit-license.php
@@ -17,34 +17,35 @@
 
   $.thermometer = {
     defaults: {
-      title_markup: false,    // if false, then uses the text of the currently active step (i.e. li.active)
-      active_class: 'active'  // in case you're using something different for your active class (e.g. current)
+      title_markup: false,     // if false, then uses the text of the currently active step (i.e. li.active)
+      active_class: 'active',  // in case you're using something different for your active class (e.g. current)
+      animate: false           // animate the thermometer on page load
     }
   }
 
   $.fn.thermometer = function(method) {
-    
-    var config = $.extend({}, $.thermometer.defaults, config);
-    
+
+    var config = $.extend({}, $.thermometer.defaults, method);
+
     var methods = {
 
       init : function(config) {
 
         return this.each(function() {
-          
+
           var thermometer = $(this).addClass('thermometer');
-          
+
           // Add stuff to the DOM
           thermometer.wrap('<div class="thermometer_wrapper"></div>');
           thermometer.before('<h4 class="thermometer_current_status"></h4>');
-          thermometer.prepend('<div class="progress_bar"></div>');
-          
+          thermometer.prepend('<div class="progress_bar_wrapper"><div class="progress_bar"></div></div>');
+
           // Create the thermometer
           helpers.build_thermometer_title(thermometer);
           helpers.build_progress_bar(thermometer);
         });
       },
-      
+
       // public methods
       update_active_state: function() {
         helpers.build_thermometer_title(this);
@@ -64,16 +65,18 @@
       },
       build_progress_bar: function(thermometer) {
         var progress_bar = thermometer.find('.progress_bar');
-        var round_to_nearest_five = function(num) {
-          return (num % 5) >= 2.5 ? parseInt(num / 5) * 5 + 5 : parseInt(num / 5) * 5;
-        }
-        var percentage_completed = function() {
+        var progress_bar_width = function() {
+          var progress_bar_wrapper_width = $('.progress_bar_wrapper').width();
           var steps = thermometer.find('li');
           var active_step_number = steps.index(helpers.active_step(thermometer)) + 1;
-          var final_step = active_step_number === steps.size();
-          return final_step ? 100 : round_to_nearest_five( (active_step_number / steps.size()) * 100 );
+          return (active_step_number / steps.size()) * progress_bar_wrapper_width;
         };
-        progress_bar.attr('class', 'progress_bar progress_' + percentage_completed());
+
+        if (config.animate) {
+          progress_bar.animate({'width': progress_bar_width()}, 1000);
+        } else {
+          progress_bar.css('width', progress_bar_width());
+        }
       }
     }
 
